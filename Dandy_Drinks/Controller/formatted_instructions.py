4e2dd59da4ec1ee19305b0d49299a0c00da4bfa2
vlovell24 <vlovell24@aol.com>
2023-01-29 16:52:08 -0500

    Preparing for Sphinx docs

--- Dandy_Drinks/Controller/formatted_instructions.py
+++ Dandy_Drinks/Controller/formatted_instructions.py
@@ -1,8 +1,22 @@
+"""
+FormattedInstructions is used as a utility/helper/mixin class. It may be used as a Mixin or as an import. Either is
+acceptable. This class is used to format the Drink Instruction field for a returned drink. Tkinters Text is used to
+create the Instructions for a drink, and the data needs to be formatted correctly. The ingredients are separated by
+two before a new line is fed in. The instructions portion of the data is then placed on the text widget after two new
+line chars. The Text widget will automatically place a word on a new line if it too long for the box. The None values
+for an ingredient measurement are then removed from the formatted document, and it is returned.
+"""
+
 
 class FormattedInstructions:
-    """A utility class/Mixin for formatting the instructions field text correctly"""
     @staticmethod
     def formatted_instructions(ingredients, data):
+        """
+        Formats the instructions text box correctly
+        :param ingredients: a list of ingredients
+        :param data: list; returned api information. The index of 5 is the instructions.
+        :return: string; the instructions for the drink
+        """
         formatted_instructions = ""
         for index, ingredient in enumerate(ingredients):
             if index % 2 == 0:
--- Dandy_Drinks/Controller/update_single_drink.py
+++ Dandy_Drinks/Controller/update_single_drink.py
@@ -1,18 +1,34 @@
 from tkinter import END
 from Dandy_Drinks.Controller.formatted_instructions import FormattedInstructions
 
+"""
+The UpdateSingleDrink class is used to change the widget values for one drink. Each page has an end result of returning
+a single updated drink, therefore this class is a helper/utility/mixin class that may be used as multi-inheritance or 
+as an import. Either is fine. This class updates the drink name, category, alcoholic or non, glass type, and
+instructions for a drink. Because the instructions widget is a Text widget, and it is disabled from modification, it 
+must first be enabled, the original values deleted, and then updated with the new values. Then it must be disabled again
+"""
+
 
 class UpdateSingleDrink:
-    """Utility class to update the values for one single drink"""
+    """
+    Used to update a single drinks widget values.
+    :return: None; updates widget values
+    :exception: IndexError, if index out of range, returns None
+    """
+
     @staticmethod
     def update_single_drink(data, drink_name, category, alcoholic, glass, instructions):
-        ingredients = data[6:]
-        drink_name['text'] = data[0]
-        category['text'] = data[1]
-        alcoholic['text'] = data[2]
-        glass['text'] = data[3]
-        instructions['state'] = 'normal'
-        instructions.delete(1.0, END)
-        new_instructions = FormattedInstructions.formatted_instructions(ingredients, data)
-        instructions.insert(END, new_instructions)
-        instructions['state'] = 'disabled'
+        try:
+            ingredients = data[6:]
+            drink_name['text'] = data[0]
+            category['text'] = data[1]
+            alcoholic['text'] = data[2]
+            glass['text'] = data[3]
+            instructions['state'] = 'normal'
+            instructions.delete(1.0, END)
+            new_instructions = FormattedInstructions.formatted_instructions(ingredients, data)
+            instructions.insert(END, new_instructions)
+            instructions['state'] = 'disabled'
+        except IndexError:
+            return None
