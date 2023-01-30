ffbc4a795b98aa1b7ca4266cb0e82eb1c39667c5
vlovell24 <vlovell24@aol.com>
2023-01-30 08:25:09 -0500

    updating documentation on title_bar.py

--- Dandy_Drinks/View/title_bar.py
+++ Dandy_Drinks/View/title_bar.py
@@ -2,6 +2,15 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from Dandy_Drinks.Images import BANNER
 
+"""
+TitleBar is a class that inherits from the tkinter Frame. This class creates the image banner on the main page and 
+packs it to the screen. The banner image supplied does not need to be presized as this class opens the image and 
+resizes it with the PIL library function 'resize'. It then applies antialias to the image (what i call jiggidy lines)
+and places the image in a tkinter label. This is then packed to whatever widget it is called from. The image is hard
+coded, but an image parameter could be added and any image could be passed in. Furthermore, the resize values could 
+also be added to the parameters and the image could be resized at any sized needed. 
+"""
+
 
 class TitleBar(ttk.Frame):
     def __init__(self, parent):
